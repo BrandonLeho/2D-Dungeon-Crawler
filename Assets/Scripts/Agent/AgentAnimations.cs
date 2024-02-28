5e701f19f77c77eb36001daab90f304570faac0f
BrandonLeho <89223038+BrandonLeho@users.noreply.github.com>
2024-02-28 00:48:03 -0800

    Did a whoopsie daisy on that last commit, I forgor directory path

---
+++ Assets/Scripts/Agent/AgentAnimations.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentAnimations : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void RotateToPointer(Vector2 lookDirection)
+    {
+        animator.SetFloat("moveX", lookDirection.x);
+        //animator.SetFloat("moveY", lookDirection.y);
+    }
+
+    public void PlayAnimation(Vector2 movementInput)
+    {
+        animator.SetBool("isMoving", movementInput.magnitude > 0);
+
+    }
+}
